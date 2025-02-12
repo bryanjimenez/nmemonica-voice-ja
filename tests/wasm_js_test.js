fc6fcebdbf22e6d7273bbe17ba15d1929fedb2f4
bryanjimenez <bryanjimenez6@gmail.com>
2025-02-12 16:08:14 -0500

    test: buildSpeech no external voice

--- tests/wasm_js_test.js
+++ tests/wasm_js_test.js
@@ -1,45 +1,24 @@
 import assert from "assert";
 import { buildSpeech } from "@nmemonica/voice-ja";
-import fs from "node:fs";
-import { dirname, normalize, sep } from "node:path";
-import { fileURLToPath } from "node:url";
 
 describe("@nmemonica/voice-ja", function () {
-  const dir = dirname(fileURLToPath(import.meta.url));
-  const root = normalize(`${dir}${sep}..`);
-
-    describe("buildSpeech throws", function () {
-        it("missing query", function () {
-            const result = () => {buildSpeech("", 0)};
-            assert.throws(result,"query is a required parameter")
-        });
-
-        it("missing voice_model", function () {
-            const result = () => {buildSpeech("", 0, "テスト")};
-            assert.throws(result,"voice_model is a required parameter")
-        });
-    });
-    
-    describe("buildSpeech", function () {
-        it("byteLength", function () {
-            const result = buildSpeech("", 0, "テスト", voice_model);
-            // console.log(JSON.stringify({byteLength:result.byteLength}))
-            
-            assert.equal(result.byteLength,147404)
-        });
+  describe("throws if", function () {
+    it("missing query", function () {
+      const result = () => {
+        buildSpeech("", 0);
+      };
+      assert.throws(result, "query is a required parameter");
     });
   });
 
   describe("buildSpeech", function () {
-    it("byteLength", function () {
-      const {
-        uid,
-        index,
-        buffer: result,
-      } = buildSpeech("", 0, "テスト", "neutral");
-      // console.log(JSON.stringify({byteLength:result.byteLength}))
-
+    it("default voice", function () {
+      const { buffer: result } = buildSpeech("", 0, "テスト");
       assert.equal(result.byteLength, 48148);
     });
-
+    it("happy voice", function () {
+      const { buffer: result } = buildSpeech("", 0, "テスト", "happy");
+      assert.equal(result.byteLength, 50746);
+    });
+  });
 });
