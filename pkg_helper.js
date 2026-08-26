4948ae22576fe0591e57dc910ee8ffa851c2f67e
bryanjimenez <bryanjimenez6@gmail.com>
2026-08-26 16:54:14 -0400

    FIX: include license link and direct deps

--- pkg_helper.js
+++ pkg_helper.js
@@ -1,11 +1,38 @@
+const { spawnSync } = require("node:child_process");
 const fs = require("node:fs");
 
 const manifestLocation = "./pkg/package.json";
 const noticeLocation = "./pkg/ThirdPartyNotice.txt";
+const noticeJSONLocation = "./pkg/ThirdPartyNotice.json";
 const testPkgLocation = "./tests/package.json";
 
+function getAllDependencyLicenses() {
+  const { status, stdout, stderr } = spawnSync("cargo", [
+    "license",
+    "--direct-deps-only",
+    "-j",
+  ]);
+
+  if (status !== 0) {
+    throw new Error(`Could not run cargo-license ${stderr.toString()}`);
+  }
+
+  const all = stdout.toString();
+  const licenses = JSON.parse(all);
+
+  console.table(
+    licenses.map((l) => ({
+      name: l.name,
+      version: l.version,
+      license: l.license,
+      authors: l.authors,
+    })),
+  );
+  return licenses;
+}
+
 function packageAdditionalFiles() {
-  const files = ["ThirdPartyNotice.txt"];
+  const files = ["ThirdPartyNotice.txt", "ThirdPartyNotice.json"];
 
   const pkg = fs.readFileSync(manifestLocation, { encoding: "utf8" });
   const manifest = JSON.parse(pkg);
@@ -22,26 +49,80 @@ function packageAdditionalFiles() {
   });
 }
 
-function aggregateThirdPartyNotice() {
-  const tohokuHeader = `--------------------------------------------------------------------------------
-htsvoice-tohoku-f01 v8e33060 - 2015 Intelligent Communication Network (Ito-Nose) Laboratory, Tohoku University
-https://github.com/icn-lab/htsvoice-tohoku-f01
---------------------------------------------------------------------------------
+const licenseLinks = {
+  MIT: "https://mit-license.org/",
+  "BSD-3-Clause": "https://opensource.org/license/bsd-3-clause",
+  "Apache-2.0": "https://www.apache.org/licenses/LICENSE-2.0",
+  "CC-BY-4.0": "https://creativecommons.org/licenses/by/4.0/",
+  ISC: "https://www.isc.org/licenses/",
 
-`;
-  const tohokuBody = fs.readFileSync("./htsvoice/tohoku-f01/COPYRIGHT.txt", {
+  "Apache-2.0 OR MIT":
+    "https://www.apache.org/licenses/LICENSE-2.0\nhttps://mit-license.org/",
+};
+
+function aggregateThirdPartyNotice() {
+  const pkg = fs.readFileSync(manifestLocation, { encoding: "utf8" });
+  const manifest = JSON.parse(pkg);
+
+  const s =
+    "================================================================================\n";
+  const tohoku_f01 = {
+    name: "htsvoice-tohoku-f01",
+    version: "v8e33060",
+    authors:
+      "Intelligent Communication Network (Ito-Nose) Laboratory, Tohoku University",
+    repository: "https://github.com/icn-lab/htsvoice-tohoku-f01",
+    licenseText: fs.readFileSync("./htsvoice/tohoku-f01/COPYRIGHT.txt", {
+      encoding: "utf8",
+    }),
+    license: "CC-BY-4.0",
+  };
+
+  const pkgDeps = getAllDependencyLicenses();
+  const dependencies = [tohoku_f01, ...pkgDeps].reduce((acc, dep) => {
+    const { name, authors } = dep;
+
+    if (manifest.name.endsWith(name) && manifest.collaborators[0] === authors) {
+      return acc;
+    }
+
+    return [...acc, dep];
+  }, []);
+
+  const licenses = dependencies.reduce(
+    (acc, info) => {
+      const { name, license, version, authors, repository, licenseText } = info;
+
+      if (license === undefined || license === null) {
+        throw new Error(
+          `Expected dependency to have license ${name} ${version}`,
+        );
+      }
+
+      const licenceLink = licenseLinks[license];
+
+      if (licenceLink === undefined) {
+        throw new Error(`Required link for ${license}`);
+      }
+
+      const depLicenseChunk = `${s}- ${name} v${version} - ${authors}\n- ${
+        repository
+      }\n${s}\n${licenseText || license}\n${licenceLink}\n\n`;
+
+      return acc + depLicenseChunk;
+    },
+    `THIRD PARTY SOFTWARE NOTICES AND INFORMATION
+Do Not Translate or Localize
+\n`,
+  );
+
+  // ThirdPartyNotice.txt
+  fs.writeFileSync(noticeLocation, licenses, {
     encoding: "utf8",
   });
 
-  const notice =
-    `THIRD PARTY SOFTWARE NOTICES AND INFORMATION
-Do Not Translate or Localize
-
-` +
-    tohokuHeader +
-    tohokuBody;
-
-  fs.writeFileSync(noticeLocation, notice, {
+  // ThirdPartyNotice.json
+  fs.writeFileSync(noticeJSONLocation, JSON.stringify(dependencies, null, 2), {
     encoding: "utf8",
   });
 }
